b3662f3cfc7f195f4a66ab0c8669f848ffe64094
root <root@ip-10-0-3-225.ec2.internal>
2024-01-18 13:46:28 +0000

    deleted three files from new test branch

--- models/audit_fact_orders.sql
+++
@@ -1,8 +0,0 @@
--- models/audit/audit_fact_orders.sql
-
-{{ config(materialized='table') }}
-{{ dbt_audit_helper.compare_column_values(
-    source_relation=ref('stg_orders'),
-    target_relation=ref('fact_orders'),
-    column_name='order_id'
-) }}
--- models/dim_products.sql
+++
@@ -1,9 +0,0 @@
--- models/dim/dim_products.sql
-
-{{ config(materialized='view') }}
-
-SELECT
-    product_id,
-    product_name,
-    category
-FROM {{ ref('stg_products') }}
